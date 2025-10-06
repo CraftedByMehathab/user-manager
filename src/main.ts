17c55c2fa34afdf58a7eda7af091c623528cd93e
CraftedByMehathab <mehathab.shaik@techwavellc.io>
2025-10-06 16:35:16 -0400

    feat(helmet): init

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  //HELMET
+  app.use(helmet());
+
+  // SWAGGER
   const config = new DocumentBuilder()
     .setTitle('User Management')
     .setDescription('Authentication and Authorization')
